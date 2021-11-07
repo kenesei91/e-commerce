7e76141a46aca2fb8565105aa3e3f151cd580d00
Ken Ilo <keneilo91@yahoo.com>
2021-11-07 15:07:06 -0600

    cloned files and set up connections

---
+++ server.js
@@ -0,0 +1,23 @@
+// installed dependencies
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+// setup Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// code below sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
